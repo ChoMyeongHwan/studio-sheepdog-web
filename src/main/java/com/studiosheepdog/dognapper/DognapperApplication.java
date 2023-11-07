dbbe354664209476c21961e5d68da0c12dc9b966
myeonghwan <1jmhcho@gmail.com>
2023-11-07 11:23:33 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/studiosheepdog/dognapper/DognapperApplication.java
@@ -0,0 +1,13 @@
+package com.studiosheepdog.dognapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DognapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DognapperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studiosheepdog/dognapper/DognapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studiosheepdog.dognapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DognapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

5536e5f8e64a7b4ab9b2c3028448ee28dcb150a2
myeonghwan <1jmhcho@gmail.com>
2023-11-22 02:54:21 +0900

    [Refactor/Board]: 게시판 관련 DTO 수정

--- src/main/java/com/studiosheepdog/dognapper/board/application/dto/WriteBoardDTO.java
+++ src/main/java/com/studiosheepdog/dognapper/board/application/dto/WriteBoardDTO.java
@@ -2,28 +2,15 @@ package com.studiosheepdog.dognapper.board.application.dto;
 
 import com.studiosheepdog.dognapper.board.domain.aggregate.enums.BoardCategory;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
+@NoArgsConstructor
 public class WriteBoardDTO {
-
-    private String title; // 제목
-    private String content; // 본문
-
-    private BoardCategory category; // 카테고리
-
-    private String writer; // 작성자 닉네임
-
-    public WriteBoardDTO() {
-    }
-
-    public WriteBoardDTO(String title, String content, BoardCategory category, String writer) {
-        this.title = title;
-        this.content = content;
-        this.category = category;
-        this.writer = writer;
-    }
+    private String title;
+    private String content;
+    private BoardCategory category;
+    private String writer;
 }

4a76e9e35c03d7768a9b8b4c6db6948dc5c02061
myeonghwan <1jmhcho@gmail.com>
2023-11-22 17:46:09 +0900

    [Refactor/Board]: 게시판 관련 컨트롤러 수정

--- src/main/java/com/studiosheepdog/dognapper/board/application/controller/BoardController.java
+++ src/main/java/com/studiosheepdog/dognapper/board/application/controller/BoardController.java
@@ -1,8 +1,10 @@
 package com.studiosheepdog.dognapper.board.application.controller;
 
+import com.studiosheepdog.dognapper.board.application.dto.BoardDTO;
 import com.studiosheepdog.dognapper.board.application.dto.WriteBoardDTO;
 import com.studiosheepdog.dognapper.board.application.service.BoardService;
-import com.studiosheepdog.dognapper.board.domain.aggregate.entity.Board;
+import com.studiosheepdog.dognapper.board.application.service.CommentService;
+import com.studiosheepdog.dognapper.board.application.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -12,11 +14,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/board")
+@RequestMapping("/boards")
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
+    private final CommentService commentService;
+    private final LikeService likeService;
 
     @GetMapping // 전체 조회
     public String listBoards(Model model, @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
@@ -24,10 +28,12 @@ public class BoardController {
         return "board/list";
     }
 
-    @GetMapping("/{id}/detail") // 게시글 상세 조회
+    @GetMapping("/{id}") // 게시글 상세 조회
     public String viewBoard(@PathVariable Long id, Model model) {
-        Board board = boardService.getBoard(id);
-        model.addAttribute("board", board);
+        BoardDTO boardDTO = boardService.getBoard(id);
+        model.addAttribute("board", boardDTO);
+        model.addAttribute("comments", commentService.getComments(id));
+        model.addAttribute("likes", likeService.getLikes(id));
         return "board/detail";
     }
 
@@ -39,25 +45,26 @@ public class BoardController {
     @PostMapping("/write") // 게시글 작성
     public String writeBoard(@ModelAttribute WriteBoardDTO writeBoardDTO) {
         boardService.writeBoard(writeBoardDTO);
-        return "redirect:/board";
+        return "redirect:/boards";
     }
 
     @GetMapping("/{id}/edit") // 게시글 수정 페이지 이동
     public String editBoardForm(@PathVariable Long id, Model model) {
-        Board board = boardService.getBoard(id);
-        model.addAttribute("board", board);
+        BoardDTO boardDTO = boardService.getBoard(id);
+        model.addAttribute("board", boardDTO);
         return "board/edit";
     }
 
     @PostMapping("/{id}/edit") // 게시글 수정
-    public String editBoard(@PathVariable Long id, WriteBoardDTO writeBoardDTO) {
+    public String editBoard(@PathVariable Long id, @ModelAttribute WriteBoardDTO writeBoardDTO) {
         boardService.updateBoard(id, writeBoardDTO);
-        return "redirect:/board/" + id + "/detail";
+        return "redirect:/boards/" + id;
     }
 
     @PostMapping("/{id}/delete") // 게시글 삭제
     public String deleteBoard(@PathVariable Long id) {
         boardService.deleteBoard(id);
-        return "redirect:/board";
+        return "redirect:/boards";
     }
+
 }

ffd4c0c6c8d7ae9257476e8736f2f827138ffd60
myeonghwan <1jmhcho@gmail.com>
2023-11-23 18:39:50 +0900

    [Refactor/Board]: 게시판 관련 Repository 수정

--- src/main/java/com/studiosheepdog/dognapper/board/domain/repository/LikeRepository.java
+++ src/main/java/com/studiosheepdog/dognapper/board/domain/repository/LikeRepository.java
@@ -1,7 +1,11 @@
 package com.studiosheepdog.dognapper.board.domain.repository;
 
+import com.studiosheepdog.dognapper.board.domain.aggregate.entity.Board;
 import com.studiosheepdog.dognapper.board.domain.aggregate.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    Optional<Like> findByBoardAndUserId(Board board, String userId);
 }
